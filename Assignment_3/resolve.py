f9aa673d9717f45962fd377f62964776a1894afb
alecaines <alecaines@gmail.com>
2021-03-18 19:04:49 -0400

    updating

--- Assignment_3/resolve.py
+++ Assignment_3/resolve.py
@@ -50,7 +50,7 @@ def collect_results(name: str) -> dict:
             cnames.append({"name": answer, "alias": name})
     # lookup A
     response = lookup(target_name, dns.rdatatype.A, ROOT_SERVERS)
-    #print('(52) response:', response)
+    print('(53) response:', response)
     arecords = []
     for answers in response.answer:
         a_name = answers.name
@@ -105,36 +105,29 @@ def lookup(target_name: dns.name.Name,
     #print('(105) testing second wave:', dns.query.udp(outbound_query, '192.5.6.3', 3))
     for server in servers:
         try:
-            t = 0 
-            while t<3:
-                time.sleep(1)
                 response = dns.query.udp(outbound_query, server, 3)
-                print(t, 'second(s) has elapsed (111)')
-                t+=1
-                print('(107) type(response)', type(response))
+                #print(t, 'second(s) has elapsed (111)')
+                #print('(107) type(response)', type(response))
                 break
             #print("(114) didn't work out")
             #return None 
         except:
             pass 
 
-    #response = dns.query.udp(outbound_query, "8.8.8.8", 3)
-    print('(120) response:', response)
+    #print('(120) response:', response)
     CNAME = []
     A = []
     AAAA = []
     MX = []
 
-    #print('(113) additional:', response.additional[0])
-    grab_ip = lambda x: str(x)[str(x).find('[<')+2:str(x).find('>]')]
+    grab_ip = lambda x: str(x)[str(x).find('[<')+2:str(x).find('>]')+1]
     remove_pre_A = lambda x: x[x.find('IN A ')+5:len(x)]
-    #print('(118) response type:', type(response))
-    #print('(119) response functions:', dir(response))
+#    print('(125) additional:', remove_pre_A(str(response.additional[0])))
     if response:
         for obj in response.additional:
             if ' A ' in str(obj):
                 #A.append(obj)
-                A.append(remove_pre_A(grab_ip(obj)))
+                A.append(remove_pre_A(str(obj)))
             elif ' AAAA ' in str(obj):
                 #AAAA.append(obj)
                 AAAA.append(grab_ip(obj))
@@ -144,19 +137,15 @@ def lookup(target_name: dns.name.Name,
             elif ' CNAME ' in str(obj):
                 #CNAME.append(obj)
                 CNAME.append(grab_ip(obj))
-    #print('(144) response:', response)
-    #print('(145) A:', A)
-    #print('(146) AAAA:', AAAA)
-    #print('(147) MX:', MX)
-    #print('(148) CNAME:', CNAME)
-    #print('(149) response type: ', type(response))
-    #if len(A) > 0:
-    print('(151) A:', A)
+    print('(139) A:', A)
     if  response.answer == []:
         #A = list(map(lambda x: x[x.find('A ')+2:len(x)-2], A))
         lookup(target_name, dns.rdatatype.A, A)
     else:
         return response
+
+
+
 #    print('(114) answer:', response.answer)
 #    types = set([obj.rdtype for obj in response.additional])
 #    print(types)
